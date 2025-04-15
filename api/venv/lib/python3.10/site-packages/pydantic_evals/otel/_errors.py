afb4d89f22da47d883bb51e678c97c9ee8e76537
Yash Thipsay <yashthpsay@gmail.com>
2025-04-15 22:55:00 +0530

    orchestration of parsing agent

--- api/venv/lib/python3.10/site-packages/pydantic_evals/otel/_errors.py
+++
@@ -1,7 +0,0 @@
-class SpanTreeRecordingError(Exception):
-    """An exception that is used to provide the reason why a SpanTree was not recorded by `context_subtree`.
-
-    This will either be due to missing dependencies or because a tracer provider had not been set.
-    """
-
-    pass
